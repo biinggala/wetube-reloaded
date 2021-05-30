bc69ab11be9374d9cb7cc7299d193c3d49e6fb43
biinggala <49203064+biinggala@users.noreply.github.com>
2021-05-30 21:06:33 +0900

    create html files with pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,15 +1,15 @@
-export const trending = (req, res) => res.send("homepage videos");
-export const search = (req, res) => res.send("search videos");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const search = (req, res) => res.render("search videos");
 export const see = (req, res) => {
   console.log(req.params);
-  return res.send("watch videos");
+  return res.render("watch", { pageTitle: " Watch Video" });
 };
 export const edit = (req, res) => {
   console.log(req.params);
-  return res.send("edit videos");
+  return res.render("edit", { pageTitle: "Edit Video" });
 };
 export const deleteVideo = (req, res) => {
   console.log(req.params);
-  return res.send("del videos");
+  return res.render("delete");
 };
-export const upload = (req, res) => res.send("upload videos");
+export const upload = (req, res) => res.render("upload");
